ff8bd589d7c51d1afa018c0074498195de21e3cc
Wyatt Baldwin <self@wyattbaldwin.com>
2019-04-23 16:07:19 -0700

    Update deploy command for Ansible-based deployments
    Update the remote directory the front end is pushed to and make a few
    internal changes to show more info about what's going on.

--- commands.py
+++ commands.py
@@ -38,8 +38,12 @@ def clean():
 
 
 @command
-def deploy(host, to='/sites/bycycle.org/frontend/', build_=True, clean_=False, overwrite=False):
+def deploy(host, to='/sites/bycycle.org/current/frontend/', build_=True, clean_=False,
+           overwrite=False, dry_run=False):
+    printer.header(f'Deploying to {host}...')
     if build_:
         build(clean_=clean_)
-    sync('build/', to, host, run_as='bycycle', delete=overwrite)
+    printer.info(f'Pushing build/ to {to}...')
+    sync('build/', to, host, run_as='bycycle', delete=overwrite, dry_run=dry_run, echo=True)
+    printer.info(f'Setting ownership of {to}...')
     remote(('chown -R bycycle:www-data', to), sudo=True)
